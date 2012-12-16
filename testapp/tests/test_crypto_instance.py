571f5543f2e025451e4f1c6ba3b6d93efbb2775d
Enrico <rico.bl@gmail.com>
2012-12-16 13:27:16 -0200

    Added crypto instance tests.

---
+++ testapp/tests/test_crypto_instance.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from django_thumbor import crypto, conf
+from django.test.utils import override_settings
+
+
+class TestCryptoInstance(TestCase):
+
+    def test_should_have_a_default_secret_key(self):
+        self.assertEqual(crypto.key, 'my-security-key')
+        self.assertEqual(crypto.key, conf.THUMBOR_SECURITY_KEY)
+
+    @override_settings(THUMBOR_SECURITY_KEY='custom-security-key')
+    def test_should_read_key_from_settings(self):
+        reload(conf)
+        self.assertEqual(conf.THUMBOR_SECURITY_KEY, 'custom-security-key')
