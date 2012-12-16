c44cae17c9fcecf46c2ec4802d20869e4fbcb231
Enrico <rico.bl@gmail.com>
2012-12-16 19:21:17 -0200

    Prepared pypi release.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 clean:
 	@find . -iname '*.pyc' -delete
+	@rm -rf dist/ django_thumbor.egg-info/
 test: clean
 	@./manage.py test
 install:
-	pip install -e .
+	@pip install -r dev_requirements.txt
+release: clean
+	@python setup.py -q sdist upload -r pyorg --show-response
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='django-thumbor',
+    version='0.1',
+    description='A django application to resize images using the thumbor service',
+    author=u'Enrico Batista da Luz',
+    author_email='rico.bl@gmail.com',
+    url='http://github.com/ricobl/django-thumbor/',
+    packages=[
+        'django_thumbor',
+        'django_thumbor.templatetags',
+    ],
+    install_requires=(
+        'Django>=1.4',
+        'libthumbor',
+    ),
+    tests_require=(
+        'django-nose',
+        'mock',
+    )
+)
