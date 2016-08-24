8b1d2f433810b223cc7389ef234dbb203f54f6d9
Henrique Chehad <hchehad@gmail.com>
2016-08-24 12:33:59 -0300

    accept model.image instance as param in templatetag thumbor_url

--- django_thumbor/templatetags/thumbor_tags.py
+++ django_thumbor/templatetags/thumbor_tags.py
@@ -25,4 +25,16 @@ def thumbor_url(image_url, **kwargs):
     filters = kwargs.get('filters')
     if filters and isinstance(filters, STRING_TYPES):
         kwargs['filters'] = _parse_filters(filters)
-    return generate_url(image_url=image_url, **kwargs)
+
+    # Accept string url and ImageField or similars classes
+    # with "url" attr as param
+    return_url = ""
+    if isinstance(image_url, STRING_TYPES):
+        return_url = image_url
+    elif image_url and hasattr(image_url, "url"):
+        return_url = getattr(image_url, "url", "")
+    else:
+        # Invalid url, not call generate_url()
+        return ""
+
+    return generate_url(image_url=return_url, **kwargs)
