1b76020f889cff410c8f08680bc32a541739c2d2
Enrico <rico.bl@gmail.com>
2017-12-10 14:33:21 -0200

    Bump version: 0.5.6 → 0.5.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-thumbor',
-    version='0.5.6',
+    version='0.5.7',
     description=(
         'A django application to resize images using the thumbor service'),
     long_description=open('README.rst').read(),
