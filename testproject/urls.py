f2d1fbb50c3ce68fa03961e057624521dcec26f8
Enrico <rico.bl@gmail.com>
2022-01-28 21:47:01 +1100

    Add testproject view serving local and external media

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,3 +1,11 @@
-from django.conf.urls import patterns
+from django.conf import settings
+from django.urls import path
+from django.conf.urls.static import static
 
-urlpatterns = patterns('',)
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
